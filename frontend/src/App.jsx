8643a86181f78f5d3246020b4f34efe72031af23
trandangnam <trandangnam056@gmail.com>
2024-10-31 20:19:00 +0700

    frontend init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,42 @@
+import { ConfigProvider, App as AntApp } from "antd";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeRoutes } from "./routes";
+
+const queryClient = new QueryClient();
 
 function App() {
+  const router = createBrowserRouter(ThemeRoutes);
 
   return (
     <>
-      <p className="text-center pt-[20px] italic"> Hello world</p>
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: "Inter",
+            borderRadius: 4,
+            controlHeight: 37,
+          },
+          components: {
+            Table: {
+              defaultProps: {
+                size: "middle",
+                bordered: true,
+                scroll: { x: "max-content" },
+              },
+            },
+          },
+          hashed: false,
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <AntApp>
+            <RouterProvider router={router} />
+          </AntApp>
+        </QueryClientProvider>
+      </ConfigProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,9 @@
+
+const HomePage = () =>{
+     return (
+          <>
+               <h1 className="italic text-center text-teal-500">This is home page</h1>
+          </>
+     )
+}
+export default HomePage
