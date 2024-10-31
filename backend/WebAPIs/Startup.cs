b8da27d0826d5e4aa8a74381d5936eb8c66f216d
trandangnam <trandangnam056@gmail.com>
2024-10-31 13:48:25 +0700

    init frontend

--- backend/WebAPIs/Startup.cs
+++ backend/WebAPIs/Startup.cs
@@ -1,20 +1,20 @@
-﻿using Asp.Versioning;
-using DataAccess;
+﻿    using Asp.Versioning;
+    using DataAccess;
 
-namespace WebAPIs
-{
-    public class Startup
+    namespace WebAPIs
     {
-        public Startup(IConfiguration configuration)
+        public class Startup
         {
-            Configuration = configuration;
-        }
+            public Startup(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
 
-        public IConfiguration Configuration { get; }
+            public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+            // This method gets called by the runtime. Use this method to add services to the container.
+            public void ConfigureServices(IServiceCollection services)
+            {
             //#region Swagger
             //services.AddSwaggerGen(c =>
             //{
@@ -27,58 +27,71 @@ namespace WebAPIs
 
             //});
             //#endregion
+            // Cấu hình CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost3000",
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:3000")
+                              .AllowAnyMethod()
+                              .AllowAnyHeader();
+                    });
+            });
 
             #region Api Versioning
             // Add API Versioning to the Project
             services.AddApiVersioning(config =>
-            {
-                // Specify the default API Version as 1.0
-                config.DefaultApiVersion = new ApiVersion(1, 0);
-                // If the client hasn't specified the API version in the request, use the default API version number 
-                config.AssumeDefaultVersionWhenUnspecified = true;
-                // Advertise the API versions supported for the particular endpoint
-                config.ReportApiVersions = true;
-            });
-            #endregion
-            services.AddApplication();
-            services.AddInfrastructure(Configuration);
-            services.AddControllers().AddNewtonsoftJson(options =>
-                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            );
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                {
+                    // Specify the default API Version as 1.0
+                    config.DefaultApiVersion = new ApiVersion(1, 0);
+                    // If the client hasn't specified the API version in the request, use the default API version number 
+                    config.AssumeDefaultVersionWhenUnspecified = true;
+                    // Advertise the API versions supported for the particular endpoint
+                    config.ReportApiVersions = true;
+                });
+                #endregion
+                services.AddApplication();
+                services.AddInfrastructure(Configuration);
+                services.AddControllers().AddNewtonsoftJson(options =>
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
+                services.AddEndpointsApiExplorer();
+                services.AddSwaggerGen();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            #region Swagger
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnionArchitecture");
-            //});
-            #endregion
-            app.UseEndpoints(endpoints =>
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
             {
-                endpoints.MapControllers();
-            });
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseRouting();
+
+                app.UseCors("AllowLocalhost3000");
+
+                app.UseAuthorization()
+                #region Swagger
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+                // specifying the Swagger JSON endpoint.
+                //app.UseSwaggerUI(c =>
+                //{
+                //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnionArchitecture");
+                //});
+                #endregion
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            }
         }
     }
-}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="text-center pt-[20px] italic"> Hello world</p>
+    </>
+  )
+}
+
+export default App
