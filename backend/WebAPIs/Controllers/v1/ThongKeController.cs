8f1bb22924b24f3ddd253734484ba104b7176d59
tien <tienphan09098@gmail.com>
2024-11-02 14:20:06 +0700

    cap nhat so du tai khoan moi khi them giao dich và thong ke theo loai giao dich trong khoang thoi gian

---
+++ backend/WebAPIs/Controllers/v1/ThongKeController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.ThongKeFeatures;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIs.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ThongKeController : BaseApiController
+    {
+        /// <summary>
+        /// Lấy dữ liệu thống kê trong khoảng thời gian
+        /// </summary>
+        /// <param name="query">Đối tượng chứa ngày bắt đầu và ngày kết thúc</param>
+        /// <returns>Danh sách giao dịch trong khoảng thời gian</returns>
+        [HttpPost("ByDateRange")]
+        public async Task<IActionResult> GetByDateRange([FromBody] ThongKeFeatures.GetThongKe query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
