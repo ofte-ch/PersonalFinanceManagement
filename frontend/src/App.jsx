df140cc48c8cec44603b416dfcef48c7a0c4108f
trandangnam <trandangnam056@gmail.com>
2024-11-03 10:59:47 +0700

    fix conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ConfigProvider, App as AntApp } from "antd";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -41,16 +40,3 @@ function App() {
 }
 
 export default App;
-=======
-
-function App() {
-
-  return (
-    <>
-      <p className="text-center pt-[20px] italic"> Hello world</p>
-    </>
-  )
-}
-
-export default App
->>>>>>> c646d00cab3561d449b1e2a1707a6fce0f575cc9
