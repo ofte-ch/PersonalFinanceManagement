461595b4720180cab6133b344f912e87f4e757d7
Male29003 <nam2309200312345@gmail.com>
2024-11-11 10:33:28 +0700

    đưa layout chính ra folder layout (để dùng cho các file khác ngoài DashboardLayout.jsx)

---
+++ frontend/src/layouts/Header.jsx
@@ -0,0 +1,36 @@
+import { Button, Space,Switch } from "antd";
+import {
+     MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    MoonOutlined,
+    SunOutlined
+   } from "@ant-design/icons";
+import { Header } from "antd/es/layout/layout";
+
+const HeaderLayout = ({ collapsed, setCollapsed,theme,setTheme }) => {
+  const changeTheme = (value) =>{
+    setTheme(value ? 'dark' : 'light');
+  }
+  return (
+    <Header className="sticky top-0 z-50 p-5 flex items-center justify-between bg-white">
+      <Space>
+          <Button
+              id="menu-btn"
+               type="text"
+               icon= {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+               onClick = {() => setCollapsed(!collapsed)}
+          />
+              <Switch
+                  checked={theme === 'dark'}
+                  onChange={changeTheme}
+                  checkedChildren={<MoonOutlined />}
+                  unCheckedChildren={<SunOutlined />}
+          />
+      </Space>
+      <Space></Space>
+      <script src="../../../test.js">
+      </script>
+    </Header>
+  );
+};
+export default HeaderLayout;
---
+++ frontend/src/layouts/HorizontalLayout.jsx
@@ -0,0 +1,31 @@
+import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import CustomSider from "./Sider";
+import HeaderLayout from "./Header";
+import FooterLayout from "./Footer";
+
+const HorizontalLayout = () =>{
+     const [collapsed, setCollapsed] = useState(false);
+     const [theme, setTheme] = useState('light'); // true for light mode, false for dark mode
+     const [th, setTh] = useState(() => {
+      const state = localStorage.getItem("theme");
+      if(state != undefined)
+        setTheme = state;
+     })
+
+     return (
+          <Layout hasSider>
+            <CustomSider collapsed={collapsed} setCollapsed={setCollapsed} theme={theme}/>
+            <Layout>
+              <HeaderLayout collapsed={collapsed} setCollapsed={setCollapsed} theme={theme} setTheme={setTheme}/>
+              <Content className="px-6 py-6 content">
+                <Outlet />
+              </Content>
+              <FooterLayout />
+            </Layout>
+          </Layout>
+        );
+}
+export default HorizontalLayout;
