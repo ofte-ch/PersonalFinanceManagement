8096d933e0b213ddda9920bb289cfcda49fd8e86
Male29003 <nam2309200312345@gmail.com>
2024-11-14 17:05:53 +0700

    thêm modal Adding

--- frontend/src/pages/manager/TransationsPage.jsx
+++ frontend/src/pages/manager/TransationsPage.jsx
@@ -1,8 +1,9 @@
-import { Table, Typography } from "antd/lib";
+import { Table, Button } from "antd/lib";
 import React, { useEffect, useState } from "react";
+import { Modal } from "antd";
 
 const TransactionsPage = () =>{
-    
+    const [openModal, setOpenModal] = useState(false);
     const [columns, setColumns] = useState([
         {
             title: "ID",
@@ -29,18 +30,43 @@ const TransactionsPage = () =>{
             dataIndex: "ghiChu"
         }
     ]);
+    // Get dữ liệu giao dịch
     const [dataSource, setDataSource] = useState([]);
     useEffect( ()=> {
         fetch("https://localhost:44318/api/v1/GiaoDich")
             .then((res) => res.json())
             .then((result) => {
                 setDataSource(result);
+                console.log(dataSource)
             })
     }, []);
 
+    // Mở modal thêm giao dịch
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    }
+    // Đóng modal thêm giao dịch
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    }
+
+
     return (
         <>
-            <Table columns={columns} dataSource={dataSource}></Table>
+        <div className="flex justify-between">
+            <Button 
+                className="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500" onClick={handleOpenModal}
+                >
+                New +
+            </Button>
+            <button type="button" className="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 ..."> New + (2)</button>
+        </div>
+
+        <Modal title="Add new transaction" open={openModal} maskClosable={false} onOk={handleCloseModal} onCancel={handleCloseModal}> 
+            <label htmlFor="name">Name</label>
+            <input id="name" type="text" />
+        </Modal>
+        <Table columns={columns} dataSource={dataSource} rowKey="id"></Table>
         </>
     )
 }
