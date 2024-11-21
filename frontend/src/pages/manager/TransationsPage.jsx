4351bbe52cb90c248a8aefc6ad5bb726af30d760
Male29003 <nam2309200312345@gmail.com>
2024-11-21 14:31:24 +0700

    Thêm table và modal

--- frontend/src/pages/manager/TransationsPage.jsx
+++ frontend/src/pages/manager/TransationsPage.jsx
@@ -1,58 +1,20 @@
-import { Table, Button } from "antd/lib";
-import React, { useEffect, useState } from "react";
-import { Modal } from "antd";
+import { Button } from "antd/lib";
+import React, { useState } from "react";
+import TransactionsTable from "~/sections/transactions/TransactionsTable"
+import AddNewTransactionModal from "~/sections/transactions/CreateModal";
+
 
 const TransactionsPage = () =>{
-    const [openModal, setOpenModal] = useState(false);
-    const [columns, setColumns] = useState([
-        {
-            title: "ID",
-            dataIndex: "id"
-        },
-        {
-            title: "Name",
-            dataIndex: "tenGiaoDich"
-        },
-        {
-            title: "Date",
-            dataIndex: "ngayGiaoDich"
-        },
-        {
-            title: "Type",
-            dataIndex: "loaiGiaoDich"
-        },
-        {
-            title: "Total",
-            dataIndex: "tongTien"
-        },
-        {
-            title: "Note",
-            dataIndex: "ghiChu"
-        }
-    ]);
-    // Get dữ liệu giao dịch
-    const [dataSource, setDataSource] = useState([]);
-    useEffect( ()=> {
-        fetch("https://localhost:7220/api/v1/GiaoDich")
-            .then((res) => res.json())
-            .then((result) => {
-                setDataSource(result);
-            })
-    }, []);
-
+    const [openAddingModal, setOpenAddingModal] = useState(true);
+    const [currentMaxId, setCurrentMaxId] = useState([]);
     // Mở modal thêm giao dịch
     const handleOpenModal = () => {
-        setOpenModal(true);
+        setOpenAddingModal(true);
     }
-    // Đóng modal thêm giao dịch
-    const handleCloseModal = () => {
-        setOpenModal(false);
-    }
-
 
     return (
         <>
-        <div className="flex justify-between">
+        <div className="flex justify-between bg-panel">
             <Button 
                 className="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500" onClick={handleOpenModal}
                 >
@@ -60,12 +22,9 @@ const TransactionsPage = () =>{
             </Button>
             <button type="button" className="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 ..."> New + (2)</button>
         </div>
+        <AddNewTransactionModal currentMaxId={currentMaxId} isOpened={openAddingModal} setOpenAddingModal={setOpenAddingModal}/>
 
-        <Modal title="Add new transaction" open={openModal} maskClosable={false} onOk={handleCloseModal} onCancel={handleCloseModal}> 
-            <label htmlFor="name">Name</label>
-            <input id="name" type="text" />
-        </Modal>
-        <Table columns={columns} dataSource={dataSource} rowKey="id"></Table>
+        <TransactionsTable/>
         </>
     )
 }
