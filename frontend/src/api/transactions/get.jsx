d4508b2e487f829de23d1ff6cac7329f88240ac7
Male29003 <nam2309200312345@gmail.com>
2024-11-28 16:27:26 +0700

    sửa api get giao dịch

--- frontend/src/api/transactions/get.jsx
+++ frontend/src/api/transactions/get.jsx
@@ -1,8 +1,14 @@
-import { api } from "~/axios/api";
 import { useQuery } from "@tanstack/react-query";
+import { api } from "~/axios/api";
 
-export const getAllTransactions = async () => {
-    const response = await api.get("/GiaoDich");
+export const getAllTransactions = async (/*{_page, _size}*/) => {
+    const response = await api.get(`/GiaoDich`);
     return response;
 };
 
+export const excuteGetAllTransactions = async(/*{_page, _size}*/) => {
+    return useQuery({
+        queryKey: ['GiaoDich'],
+        queryFn: () => getAllTransactions(/*_page, _size*/),
+    })
+}
